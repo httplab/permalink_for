95674b23a75c8d923b7e473ad7908cadb2b4a89a
sk <sk@httplab.ru>
2016-02-24 05:34:20 +0300

    Add slug and pretty permalink implementations

--- lib/permalink_for.rb
+++ lib/permalink_for.rb
@@ -1,1 +1,28 @@
+require 'permalink_for/pretty_implementation'
+require 'permalink_for/slug_implementation'
 require 'permalink_for/version'
+
+module PermalinkFor
+  extend ActiveSupport::Concern
+
+  PERMALINK_TYPES = {
+    slug: SlugImpl,
+    pretty: PrettyImpl
+  }
+
+  module ClassMethods
+    def permalink_for(field, as: :pretty)
+      as = as.to_sym
+      unless PERMALINK_TYPES.has_key? as
+        raise "Unknown permalink type :#{as} (may be :slug or :pretty)"
+      end
+      include PERMALINK_TYPES[as]
+
+      self::ActiveRecord_AssociationRelation.include PERMALINK_TYPES[as]::ClassMethods
+
+      config = { target_field: field }
+      cattr_reader :permalink_configuration
+      class_variable_set(:@@permalink_configuration, config)
+    end
+  end
+end
